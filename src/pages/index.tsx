1f39a39dac5eea8c2438d6189222c3a84b2df4f8
GotSteven <s1f102300481@iniad.org>
2023-06-14 23:45:02 +0900

    途中まで説明書いた

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,29 +26,35 @@ const Home = () => {
   const onClick = (x: number, y: number) => {
     console.log(x, y);
     const newBoard: number[][] = JSON.parse(JSON.stringify(board));
-    let p = false;
+    let putStone = false; //石を置いたか判定する変数
     if (board[y][x] === 0) {
+      //クリックした場所に石がないなら
       for (const [dy, dx] of directions) {
+        //方向をひとつずつ、xとyに分けて取り出す
         if (
           board[y + dy] !== undefined &&
           board[y + dy][x + dx] !== undefined &&
-          board[y + dy][x + dx] === 3 - turnColor
+          board[y + dy][x + dx] === 3 - turnColor //取り出した方向に相手の石があるなら
         ) {
-          for (let i = 1; i < 8; i++) {
+          for (let i = 2; i < 8; i++) {
+            //その方向を2~7倍するためのi
             if (
               board[y + dy * i] !== undefined &&
               board[y + dy * i][x + dx * i] !== undefined &&
-              board[y + dy * i][x + dx * i] === turnColor
+              board[y + dy * i][x + dx * i] === turnColor //その方向のi倍の位置に自分の石があるなら
             ) {
-              let a = 0;
-              for (let i2 = 1; i2 <= i; i2++) {
+              const count = 0; //石のある場所の数をカウントする変数
+
+              for (let i2 = 2; i2 <= i; i2++) {
+                //2から自分の石ある位置のiまで繰り返すi2
                 if (board[y + dy * i2][x + dx * i2] !== 0) {
-                  a++;
+                  //方向のi2倍の位置に石があるなら
+                  count;
                 }
               }
-              if (a === i) {
+              if (count === i) {
                 newBoard[y][x] = turnColor;
-                p = true;
+                putStone = true;
                 for (let i2 = 1; i2 < i; i2++) {
                   newBoard[y + dy * i2][x + dx * i2] = turnColor;
                 }
@@ -58,7 +64,7 @@ const Home = () => {
           }
         }
       }
-      if (p) {
+      if (putStone) {
         setTurnColor(3 - turnColor);
         setBoard(newBoard);
       }
